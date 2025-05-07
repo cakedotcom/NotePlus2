dec624a6bc58c86a3e1464b48c9dcc6e9ec5801a
Dale-devv <Thisiskei07@gmail.com>
2025-05-07 20:32:50 +0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div className='text-2xl'>App</div>
+  )
+}
+
+export default App
